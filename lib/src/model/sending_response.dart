e2fbc28c5023faa17de3deaa9e45bf6341e6b3a8
Sahad M P <sahadmp@Sahads-Mac-mini.local>
2025-12-04 19:26:57 +0530

    adding isOutOfOfficeTime in chatSendResponse

--- lib/src/model/sending_response.dart
+++ lib/src/model/sending_response.dart
@@ -2,16 +2,34 @@ class ChatSentResponse {
   String? message;
   bool status;
   String? error;
-  ChatSentResponse({this.message, required this.status, this.error});
+  bool? isOutOfOfficeTime;
+
+
+  ChatSentResponse(
+      {this.message,
+      required this.status,
+      this.error,
+      required this.isOutOfOfficeTime});
 
   factory ChatSentResponse.fromJson(Map<String, dynamic> json) {
+    final isOutOfOfficeTime = json["out_off_hour"];
+  
+    if (isOutOfOfficeTime) {
+      return ChatSentResponse(
+        status: true,
+        message: json['content']?['message'] ?? 'Out of office time',
+        isOutOfOfficeTime: true,
+      );
+    }
     return ChatSentResponse(
       message: json['message'],
       status: json['status'],
+      isOutOfOfficeTime: false,
     );
   }
 
   factory ChatSentResponse.error(String error) {
-    return ChatSentResponse(status: false, error: error);
+    return ChatSentResponse(
+        status: false, error: error, isOutOfOfficeTime: false);
   }
 }
